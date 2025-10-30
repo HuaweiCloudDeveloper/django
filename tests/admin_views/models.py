82732010b2feec3f087d07362a67a2903c2ab7d0
pangpang20 <676814828@qq.com>
2025-10-30 13:15:06 +0800

    Simplify Article __str__ to return title directly

--- tests/admin_views/models.py
+++ tests/admin_views/models.py
@@ -47,7 +47,7 @@ class Article(models.Model):
     )
 
     def __str__(self):
-        return str(self.title) if self.title else f"Article #{self.pk or 'unsaved'}"
+        return self.title
 
     @admin.display(ordering="date", description="")
     def model_year(self):
